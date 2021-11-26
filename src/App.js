2112a2df502b2777e3e0a940a8e6bc2073e3d591
Krishna kumar <2018ugcs077@nitjsr.ac.in>
2021-11-26 19:25:18 +0530

    fixed cores

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
+import {useDispatch} from 'react-redux'
+
+import {getPosts} from "./actions/posts"
+import memories from "./images/memories.jpg";
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import useStyles from './styles';
+
 
 const App=()=>{
+    const classes=useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+
     return(
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxidth="lg">
+            <AppBar position="static" color="inherit">
+                    <Typography variant="h2" align="center">Memories</Typography>
+                    <img src={memories} alt="memories" height="60"/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spaceing="3">
+                        <Grid item xs={12} sm={7}>
+                            <Posts></Posts>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form></Form>
+                        </Grid>
+                            
+                    </Grid>
+                </Container>
+            </Grow>    
+        </Container>
+        
     );
 }
 export default App;
---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,16 @@
+//rafce
+
+ import React from 'react'
+ import useStyles from './styles';
+
+ const Post = () => {
+     const classes=useStyles();
+     return (
+         <div>
+            <h1>POST</h1>             
+         </div>
+     )
+ }
+ 
+ export default Post
+ 
